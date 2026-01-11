95f4a3daafbe3758cb369c505fa89f76d26d6976
Moremsh <muhammedremsh@hotmail.com>
2026-01-11 04:31:57 +0200

    feat: Add `AppConfig` with Supabase credentials and remove it from gitignore

---
+++ lib/core/config/app_config.dart
@@ -0,0 +1,4 @@
+class AppConfig {
+  static const SUPABASE_URL =  'https://ntwgqrlvbsrlcetangbf.supabase.co';
+  static const SUPABASE_ANON_KEY =  'sb_publishable_NNmt0Ty-1FjDR9-KEsM1zg_-0P1l_05';
+}
