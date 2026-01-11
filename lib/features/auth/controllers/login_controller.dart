750e11f33476c4602da74399c5661e02bb1146c9
Moremsh <muhammedremsh@hotmail.com>
2026-01-11 03:35:55 +0200

    feat: Implement login functionality and user model

---
+++ lib/features/auth/controllers/login_controller.dart
@@ -0,0 +1,45 @@
+import 'package:employee_data_management/data/repositories/auth_repository.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../core/utils/popups/snackbars.dart';
+import '../../employee/screens/home_screen.dart';
+
+class LoginController extends GetxController{
+  static LoginController get instance => Get.find();
+
+  final SnackBars snackBars = SnackBars();
+
+  final email = TextEditingController();
+  final password = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final isLoading = false.obs;
+  final hidePass = true.obs ;
+
+  Future<void> signInWithEmailAndPassword()async{
+    try{
+      if(!formKey.currentState!.validate()) return ;
+
+      final res = await AuthRepository.instance.signInWithEmail(email.text, password.text);
+      if(res.user != null){
+        email.text = '';
+        password.text = '';
+        Get.offAll(()=> HomeScreen());
+      }
+    }catch(error){
+      snackBars.errorSnack("Something went wrong ${error.toString()}", "Error");
+      throw "Something went wrong ${error.toString()}";
+    }
+  }
+
+  Future<void> signOut()async{
+    try{
+      await AuthRepository.instance.signOut();
+    }catch(error){
+      throw "Something went wrong ${error.toString()}";
+    }
+  }
+
+
+}
---
+++ lib/features/auth/models/user_model.dart
@@ -0,0 +1,22 @@
+class UserModel{
+  final String? id ;
+  final String? email;
+
+  UserModel({required this.id , required this.email});
+
+  UserModel empty() => UserModel(id: '', email: '');
+
+  Map<String,dynamic> toJson(UserModel user){
+    return{
+      'id' : user.id,
+      'email' : user.email,
+    };
+  }
+
+  factory UserModel.fromJson(Map<String,dynamic> json){
+    return UserModel(
+        id: json['id'],
+        email: json['email']
+    );
+  }
+}
