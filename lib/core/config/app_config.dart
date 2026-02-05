7d449a385350225233fa4907d01a5db35cbf3d15
Moremsh <muhammedremsh@hotmail.com>
2026-02-05 15:44:20 +0300

    Update Supabase URL to use environment variable

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,4 +1,4 @@
 class AppConfig {
-  static const SUPABASE_URL =  'https://ntwgqrlvbsrlcetangbf.supabase.co';
+  static const SUPABASE_URL =  ${{secrets.SUPABASE_URL}};
   static const SUPABASE_ANON_KEY =  ${{ secrets.SUPABASE_ANON_KEY }}
 }
